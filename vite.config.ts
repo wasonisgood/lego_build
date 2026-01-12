abd4bbc96e80c1df58747aca4c6bd297a20dbca2
wasonisgood <43530227+wasonisgood@users.noreply.github.com>
2026-01-12 20:16:26 +0800

    更新

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/lego_build/', // 設定 GitHub Pages 的路徑
+})
